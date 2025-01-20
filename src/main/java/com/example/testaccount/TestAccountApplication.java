a146e567f00e721c728078dc598152e8e3ed7a2a
Mauricio <m4dicio@gmail.com>
2025-01-20 17:18:15 -0500

    first commit

---
+++ src/main/java/com/example/testaccount/TestAccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.testaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAccountApplication.class, args);
+    }
+
+}
